69c2186c7c3c6dd4f73f23a3759a2a350a5aeb6e
LuciaHua <jay40739@gmail.com>
2022-03-01 23:39:02 +0800

    modify

--- paper_scissor_stone.py
+++ paper_scissor_stone.py
@@ -11,7 +11,7 @@ while True:
 print ('我出的是', a)
 if a == c:
 	print ('平手!')
-elif (a == '布' and b == '石頭') or (a == '剪刀' and b == '布') or ( a == '石頭' and b == '剪刀'):
+elif (c == '布' and a == '石頭') or (c == '剪刀' and a == '布') or ( c == '石頭' and a == '剪刀'):
 	print ('恭喜你贏啦!')
 else:
 	print ('真可惜~再接再厲吧!')
